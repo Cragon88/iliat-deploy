f6bf1d8e103970aabc0fd15653623f050dab71c9
Cuong Nguyen <cuongnt53@gmail.com>
2015-12-28 10:32:48 +0700

    dada

---
+++ client/assets/constant.caecc5fb.js
@@ -0,0 +1,124 @@
+var App = {
+
+  pageTitle: {
+    home: "Home",
+  },
+
+  button: {
+    learnMore : "Learn More",
+    detail : "Detail",
+    search: "Search",
+    startMyApplication: "Start My Application",
+    beAMomentumPartner: "Be a Momentum Partner"
+  },
+
+  homeMenu: {
+    item1 : {name: "Programs", link: "/programs"},
+    item2 : {name: "Partners", link: "/partners"},
+    item3 : {name: "About Us", link: "/about-us"},
+    item4 : {name: "Donate", link: "/donate"}
+  },
+
+
+  footer: {
+    menu: ["Trang chủ", "Giới thiệu", "Sứ giả", "Khóa học", "Tin tức", "Liên hệ", "Tuyển dụng"],
+    follow: "Follow us on",
+    contacts: [
+      {
+        address: "Số 18, Ngõ 67, Chùa láng, Đống đa",
+        phone: "Ms. Quỳnh: 0165 339 9457"
+      },
+      {
+        address: "Số 28, Ngách 10, Ngõ 121, Chùa láng, Đống đa",
+        phone: ""
+      },
+      {
+        address: "Số 101d, Ngõ 95, Chùa bộc, Đống đa",
+        phone: "Ms. Hà: 0165 300 5670"
+      },
+      {
+        address: "Số 13, Ngõ 83, Trần đại nghĩa, Hai Bà Trưng",
+        phone: ""
+      }
+    ]
+  },
+
+  home : {
+
+    vision: {
+      title: "Ignite and shine",
+      items: [
+        {
+          image: "/assets/images/home/kimcuong.41402eea.png",
+          name:"Xuất Sắc",
+          description: "Không chấp nhận những chuẩn mực thông thường, tất cả điều chúng tôi là trau dồi từng cá nhân để giúp họ trở nên xuất sắc và tạo ra giá trị đặc biệt"
+        },
+        {
+          image: "/assets/images/home/khacbiet.89a7c381.png",
+          name:"Khác Biệt",
+          description: "Một môi trường mà ở đó bất cứ ai cũng được là chính mình, được tự do làm những điều mình muốn và theo đuổi thứ mình đam mê"
+        },
+        {
+          image: "/assets/images/home/chiase_ketnoi.ea702583.png",
+          name:"Sẻ chia và kết nối",
+          description: "Chúng tôi xây dựng một cộng đồng mà ai cũng đều khiêm tốn, mở lòng học hỏi và chia sẻ"
+        }
+      ]
+
+    },
+    courses: {
+      title: "Các Khóa học",
+      items: [
+        {
+          image: "/assets/images/home/courses/khoahocgmat.jpg",
+          title: "Khóa học Gmat",
+          content: "Hiện thực hóa giấc mơ học kinh tế"
+        },
+        {
+          image: "/assets/images/home/courses/khochocielts.jpg",
+          title: "Khóa học ielts",
+          content: "Tấm pasport của bạn"
+        },
+        {
+          image: "/assets/images/home/courses/khoahoccfa.jpg",
+          title: "Khóa học cfa",
+          content: "Trở thành nhà đầu tư tài chính chuyên nghiệp"
+        },
+        {
+          image: "/assets/images/home/courses/khoahocgre.jpg",
+          title: "Khóa học gre",
+          content: "Tấm visa trên miền đất hứa"
+        },
+        {
+          image: "/assets/images/home/courses/khoahoctoefl.jpg",
+          title: "Khóa học toefl",
+          content: "Giới thiệu khóa học toefl"
+        },
+        {
+          image: "/assets/images/home/courses/khochocacca.jpg",
+          title: "Khóa học acca",
+          content: "BIG 4 trong tầm tay"
+        }
+      ]
+    },
+
+    news: {
+      title: "Tin tức mới",
+      images: [
+        "https://placehold.it/600x400",
+        "https://placehold.it/600x400",
+        "https://placehold.it/600x400"
+      ]
+    },
+
+    bottom: {
+      connect: {
+        title: "Kết nối với chúng tôi",
+        image: "/assets/images/home/connect-us.9eabc777.png",
+        link: "https://www.facebook.com/ILIAT.Education"
+      },
+
+      video: "Video"
+    }
+  }
+};
